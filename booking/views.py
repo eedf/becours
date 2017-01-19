970ea62fc2c60d7818d6c3609abd7cf9cdbd2db4
Gael UTARD <gael.utard@kisis.fr>
2017-01-19 11:37:40 +0100

    Add list of bookings to occupancy view

--- booking/views.py
+++ booking/views.py
@@ -12,13 +12,14 @@ class OccupancyView(TemplateView):
 
     def occupancy_for(self, day):
         items = BookingItem.objects.filter(begin__lte=day, end__gt=day, product=1, headcount__isnull=False)
-        return sum(items.values_list('headcount', flat=True))
+        items = items.order_by('headcount', 'booking__title')
+        return (sum([item.headcount for item in items]), items)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         occupancy = []
         for i in range(365):
             day = date(2017, 1, 1) + timedelta(days=i)
-            occupancy.append((day, self.occupancy_for(day)))
+            occupancy.append((day, ) + self.occupancy_for(day))
         context['occupancy'] = occupancy
         return context

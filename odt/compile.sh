062a948b0a02fd20101ca8e07e48c2c016b7ae63
Gaël UTARD <gael.utard@makina-corpus.com>
2017-01-21 08:17:07 +0100

    Basic odt structure

---
+++ odt/compile.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+rm ../booking/templates/booking/agreement.odt
+zip ../booking/templates/booking/agreement.odt mimetype META-INF/manifest.xml content.xml meta.xml settings.xml styles.xml
+

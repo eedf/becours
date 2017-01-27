0f5a4052fa1a479809d4335c6388dfbc31a5f2b5
Gaël UTARD <gael.utard@makina-corpus.com>
2017-01-27 17:31:36 +0100

    WIP on agreement template

--- odt/compile.sh
+++ odt/compile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 rm ../booking/templates/booking/agreement.odt
-zip ../booking/templates/booking/agreement.odt mimetype META-INF/manifest.xml content.xml meta.xml settings.xml styles.xml
+zip ../booking/templates/booking/agreement.odt mimetype META-INF/manifest.xml content.xml meta.xml settings.xml styles.xml logo.png signature.png
 
